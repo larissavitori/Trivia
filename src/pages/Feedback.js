5c55557e21c260428678178d1b1b5f4a30da7291
larissa <laryterra@gmail.com>
2023-02-12 11:53:53 -0300

    requisito Feito

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
 import '../style/Feedback.css';
 
 class Feedback extends Component {
@@ -31,10 +32,24 @@ class Feedback extends Component {
 
   render() {
     const { message, type } = this.state;
-    const { score, assertions } = this.props;
+    const { score, assertions, name, email } = this.props;
+    const hashGerada = md5(email).toString();
+    const img = `https://www.gravatar.com/avatar/${hashGerada}`;
     return (
       <div className="feedback">
         <div className="feedback-container">
+          <img
+            data-testid="header-profile-picture"
+            src={ img }
+            className="gravatar"
+            alt="gravatar"
+          />
+          <p data-testid="header-player-name">{name}</p>
+          <p>score:</p>
+          <p data-testid="header-score">
+            { score }
+          </p>
+          <h1 data-testid="feedback-text">{message}</h1>
           <h1 data-testid="feedback-text" className={ `message ${type}` }>
             {message}
           </h1>
@@ -80,11 +95,15 @@ class Feedback extends Component {
 const mapStateToProps = (state) => ({
   assertions: state.player.assertions,
   score: state.player.score,
+  name: state.player.name,
+  email: state.player.email,
 });
 
 Feedback.propTypes = {
   assertions: PropTypes.number.isRequired,
   score: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
